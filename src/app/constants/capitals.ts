94d111b95e2213eea15cb464e3e1e22f70ce821f
Misha Shmyhelskyy <shmyhelskyy.misha@gmail.com>
2018-11-13 11:40:06 +0200

    add capitals

--- src/app/constants/capitals.ts
+++ src/app/constants/capitals.ts
@@ -28,7 +28,233 @@ const capitals = {
         name: 'Minsk',
         country: 'Belarus',
         id: 625144,
-    }
+    },
+    Brussels: {
+        name: 'Brussels',
+        country: 'Belgium',
+        id: 2800866,
+    },
+    Sarajevo: {
+        name: 'Sarajevo',
+        country: 'Sarajevo',
+        id: 3191281,
+    },
+    Sofia: {
+        name: 'Sofia',
+        country: 'Bulgaria',
+        id: 727011,
+    },
+    Zagreb: {
+        name: 'Zagreb',
+        country: 'Croatia',
+        id: 3186886,
+    },
+    Nicosia: {
+        name: 'Nicosia',
+        country: 'Cyprus',
+        id: 146268,
+    },
+    Prague: {
+        name: 'Prague',
+        country: 'Czech Republic',
+        id: 3067696,
+    },
+    Copenhagen: {
+        name: 'Copenhagen',
+        country: 'Denmark',
+        id: 2618425,
+    },
+    Tallinn: {
+        name: 'Tallinn',
+        country: 'Estonia',
+        id: 588409,
+    },
+    Helsinki: {
+        name: 'Helsinki',
+        country: 'Finland',
+        id: 658225,
+    },
+    Paris: {
+        name: 'Paris',
+        country: 'France',
+        id: 6455259,
+    },
+    Tbilisi: {
+        name: 'Tbilisi',
+        country: 'Georgia',
+        id: 625144,
+    },
+
+    Berlin: {
+        name: 'Berlin',
+        country: 'Germany',
+        id: 2950158,
+    },
+    Athens: {
+        name: 'Athens',
+        country: 'Greece',
+        id: 264371,
+    },
+    Budapest: {
+        name: 'Budapest',
+        country: 'Hungary',
+        id: 3054643,
+    },
+    Reykjavik: {
+        name: 'Reykjavik',
+        country: 'Iceland',
+        id: 6692263,
+    },
+    Dublin: {
+        name: 'Dublin',
+        country: 'Ireland',
+        id: 2964574,
+    },
+    Rome: {
+        name: 'Rome',
+        country: 'Italy',
+        id: 3169070,
+    },
+    Astana: {
+        name: 'Astana',
+        country: 'Kazakhstan',
+        id: 1526273,
+    },
+    Pristina: {
+        name: 'Pristina',
+        country: 'Kosovo',
+        id: 786714,
+    },
+    Riga: {
+        name: 'Riga',
+        country: 'Latvia',
+        id: 456172,
+    },
+    Vaduz: {
+        name: 'Vaduz',
+        country: 'Liechtenstein',
+        id: 3042030,
+    },
+    Vilnius: {
+        name: 'Vilnius',
+        country: 'Lithuania',
+        id: 593116,
+    },
+    Luxembourg: {
+        name: 'Luxembourg',
+        country: 'Luxembourg',
+        id: 2960316,
+    },
+    Skopje: {
+        name: 'Skopje',
+        country: 'Macedonia',
+        id: 785842,
+    },
+    Valletta: {
+        name: 'Valletta',
+        country: 'Malta',
+        id: 2562305,
+    },
+    Chisinau: {
+        name: 'Chisinau',
+        country: 'Moldova',
+        id: 618426,
+    },
+    Monaco: {
+        name: 'Monaco',
+        country: 'Monaco',
+        id: 2993458,
+    },
+    Podgorica: {
+        name: 'Podgorica',
+        country: 'Montenegro',
+        id: 3193044,
+    },
+    Oslo: {
+        name: 'Oslo',
+        country: 'Norway',
+        id: 6453366,
+    },
+    Amsterdam: {
+        name: 'Amsterdam',
+        country: 'Netherlands',
+        id: 2759794,
+    },
+    Warsaw: {
+        name: 'Warsaw',
+        country: 'Poland',
+        id: 756135,
+    },
+    Lisbon: {
+        name: 'Lisbon',
+        country: 'Portugal',
+        id: 2267057,
+    },
+    Bucharest: {
+        name: 'Bucharest',
+        country: 'Romania',
+        id: 683506,
+    },
+    Moscow: {
+        name: 'Moscow',
+        country: 'Russia',
+        id: 524901,
+    },
+    'San Marino': {
+        name: 'San Marino',
+        country: 'San Marino',
+        id: 3168070,
+    },
+    Belgrade: {
+        name: 'Belgrade',
+        country: 'Serbia',
+        id: 792680,
+    },
+    Bratislava: {
+        name: 'Bratislava',
+        country: 'Slovakia',
+        id: 3060972,
+    },
+    Ljubljana: {
+        name: 'Ljubljana',
+        country: 'Slovenia',
+        id: 3196359,
+    },
+    Madrid: {
+        name: 'Madrid',
+        country: 'Spain',
+        id: 6359304,
+    },
+    Stockholm: {
+        name: 'Stockholm',
+        country: 'Sweden',
+        id: 2673730,
+    },
+    Bern: {
+        name: 'Bern',
+        country: 'Switzerland',
+        id: 7285212,
+    },
+    Ankara: {
+        name: 'Ankara',
+        country: 'Turkey',
+        id: 323786,
+    },
+    Kyiv : {
+        name: 'Moscow',
+        country: 'Ukraine',
+        id: 703448,
+    },
+    London: {
+        name: 'London',
+        country: 'United Kingdom',
+        id: 2643743,
+    },
+    'Vatican City': {
+        name: 'Vatican City',
+        country: 'Vatican City',
+        id: 6691831,
+    },
 };
 
 export { capitals };
